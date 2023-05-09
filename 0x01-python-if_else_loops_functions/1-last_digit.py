911e4b85fbb9769a2c97a328be3f1a90c2f99b0e
KwameNtaadu007 <kwamealabama@gmail.com>
2023-05-09 08:27:53 -0700

    indentation and code refactor

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-if number > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number == 0:
-    print(f"Last digit of {number} is {number} and is 0")
-elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {number %-10}"
-    " and is less than 6 and not 0")
+last_digit = abs(number) % 10
+
+if last_digit > 5:
+    message = f"Last digit of {number} is {last_digit} and is greater than 5"
+elif last_digit == 0:
+    message = f"Last digit of {number} is {last_digit} and is 0"
+else:
+    message = f"Last digit of {number} is {last_digit} and is less than 6 and not 0"
+
+print(message)
