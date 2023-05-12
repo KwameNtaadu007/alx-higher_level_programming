b56bfff33daeb2f00728124165039215d0497242
KwameNtaadu007 <kwamealabama@gmail.com>
2023-05-11 22:40:26 -0700

    print python is cool

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
-        print("Operator unknown. Available operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
