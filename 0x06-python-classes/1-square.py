7e141388cdc37e1719100a44391d570ff31826e3
KwameNtaadu007 <kwamealabama@gmail.com>
2023-05-30 17:14:26 -0700

    empty class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        """
+
+        self.__size = size
