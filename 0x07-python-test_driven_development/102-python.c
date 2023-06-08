d1543f842d2cb2c4b749bede2dbaba7c1490366c
KwameNtaadu007 <kwamealabama@gmail.com>
2023-06-08 02:31:41 -0700

    created a function that prints python strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,29 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+    long int length;
+
+    fflush(stdout);
+
+    printf("[.] string object info\n");
+    if (!p || !PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    length = PyUnicode_GetLength(p);
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
+    printf("  length: %ld\n", length);
+    printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
+
