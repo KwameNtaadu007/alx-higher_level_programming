b9a659f9241f9897adabc2d64646b9853c9431a3
KwameNtaadu007 <kwamealabama@gmail.com>
2023-06-12 15:46:18 -0700

    check instance of a specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+def is_same_class(obj, a_class):
+    """
+    Check if an object is an instance of a specific class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of the specified class, False otherwise.
+    """
+    # Compare the type of the object with the specified class
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
+
