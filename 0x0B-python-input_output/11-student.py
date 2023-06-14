ebd3712e9092f0896b5e56a445a51a08f1c93ded
KwameNtaadu007 <kwamealabama@gmail.com>
2023-06-13 22:46:16 -0700

    a build up of 10-studend

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains the class "Student"
+"""
+
+
+class Student:
+    """Representation of a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance from a dictionary"""
+        for key, value in json.items():
+            setattr(self, key, value)
+
