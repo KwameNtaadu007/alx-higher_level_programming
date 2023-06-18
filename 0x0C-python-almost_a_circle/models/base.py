1aa852eb6af70f87177f357dcc7c634a9ffb047f
KwameNtaadu007 <kwamealabama@gmail.com>
2023-06-18 13:44:12 -0700

    added from_json_string method to the Bass class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,3 +57,18 @@ class Base:
         json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
         with open(filename, 'w') as file:
             file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by the JSON string.
+
+        Args:
+            json_string (str): A string representing a list of dictionaries.
+
+        Returns:
+            list: The list represented by the JSON string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
