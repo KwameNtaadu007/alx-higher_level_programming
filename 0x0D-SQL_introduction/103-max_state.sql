39a0446924263c0c443a6b501730bffee61c1a0e
KwameNtaadu007 <kwamealabama@gmail.com>
2023-10-18 15:38:29 -0700

    more queries

--- 0x0D-SQL_introduction/103-max_state.sql
+++
@@ -1,3 +0,0 @@
--- Script which displays the max temperature(Fahrenheit) of each state (ordered by State name).
-
-SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++
@@ -1,3 +0,0 @@
--- Script to list all tables of a database in MySQL server.
-
-SHOW TABLES;
