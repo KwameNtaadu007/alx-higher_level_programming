0cdfef27c76c7201009cbe6a511e623f7b6c5bbc
KwameNtaadu007 <kwamealabama@gmail.com>
2023-11-13 21:28:54 -0800

    addition function

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,17 +1,2 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-
-myObject.incr = function () {
-  this.value++;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+exports.add = (a, b) => a + b;
